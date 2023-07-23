ba7e81ef9d85d894e13551130452564b95a54d2f
redsonw <seeyyy@qq.com>
2023-07-23 23:14:12 +0800

    📓创建日志类...

---
+++ src/Utilities/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace YueHuan
+{
+    public class LogMessage
+    {
+        private readonly ListBox loggerListBox;
+
+        public LogMessage(ListBox loggerListBox)
+        {
+            this.loggerListBox = loggerListBox;
+        }
+
+        public void Add(string message)
+        {
+            loggerListBox.Items.Add(message);
+            loggerListBox.SelectedIndex = loggerListBox.Items.Count - 1;
+            loggerListBox.ClearSelected();
+            loggerListBox.TopIndex = loggerListBox.Items.Count - 1;
+        }
+    }
+
+}
