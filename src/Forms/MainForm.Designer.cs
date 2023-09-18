fdbefc508c46ebf73cd1c0646f889cbd77dae22f
弦月 <seeyyy@qq.com>
2023-09-18 23:13:00 +0800

    Update MainForm.Designer.cs

--- src/Forms/MainForm.Designer.cs
+++ src/Forms/MainForm.Designer.cs
@@ -47,54 +47,59 @@
             // 
             LoggerListBox.Dock = DockStyle.Fill;
             LoggerListBox.FormattingEnabled = true;
-            LoggerListBox.ItemHeight = 17;
-            LoggerListBox.Location = new Point(3, 19);
+            LoggerListBox.ItemHeight = 31;
+            LoggerListBox.Location = new Point(6, 36);
+            LoggerListBox.Margin = new Padding(6, 5, 6, 5);
             LoggerListBox.Name = "LoggerListBox";
-            LoggerListBox.Size = new Size(437, 147);
+            LoggerListBox.Size = new Size(874, 267);
             LoggerListBox.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 12);
+            label1.Location = new Point(24, 22);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(72, 17);
+            label1.Size = new Size(140, 31);
             label1.TabIndex = 1;
             label1.Text = "[ 最新版本 ]";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 41);
+            label2.Location = new Point(24, 75);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(72, 17);
+            label2.Size = new Size(140, 31);
             label2.TabIndex = 2;
-            label2.Text = "[ 下载地址 ]";
+            label2.Text = "[ 下载微信 ]";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 70);
+            label3.Location = new Point(24, 128);
+            label3.Margin = new Padding(6, 0, 6, 0);
             label3.Name = "label3";
-            label3.Size = new Size(72, 17);
+            label3.Size = new Size(140, 31);
             label3.TabIndex = 3;
             label3.Text = "[ 补丁信息 ]";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 99);
+            label5.Location = new Point(24, 181);
+            label5.Margin = new Padding(6, 0, 6, 0);
             label5.Name = "label5";
-            label5.Size = new Size(72, 17);
+            label5.Size = new Size(140, 31);
             label5.TabIndex = 5;
             label5.Text = "[ 发布时间 ]";
             // 
             // PatchesButton
             // 
-            PatchesButton.Location = new Point(364, 64);
+            PatchesButton.Location = new Point(728, 117);
             PatchesButton.Margin = new Padding(0);
             PatchesButton.Name = "PatchesButton";
-            PatchesButton.Size = new Size(90, 55);
+            PatchesButton.Size = new Size(180, 100);
             PatchesButton.TabIndex = 11;
             PatchesButton.Text = "解除限制";
             PatchesButton.UseVisualStyleBackColor = true;
@@ -103,18 +108,20 @@
             // VersionLabel
             // 
             VersionLabel.BorderStyle = BorderStyle.Fixed3D;
-            VersionLabel.Location = new Point(90, 9);
+            VersionLabel.Location = new Point(180, 16);
+            VersionLabel.Margin = new Padding(6, 0, 6, 0);
             VersionLabel.Name = "VersionLabel";
-            VersionLabel.Size = new Size(365, 23);
+            VersionLabel.Size = new Size(730, 42);
             VersionLabel.TabIndex = 12;
             VersionLabel.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // DownloadLinkLabel
             // 
             DownloadLinkLabel.BorderStyle = BorderStyle.Fixed3D;
-            DownloadLinkLabel.Location = new Point(90, 38);
+            DownloadLinkLabel.Location = new Point(180, 69);
+            DownloadLinkLabel.Margin = new Padding(6, 0, 6, 0);
             DownloadLinkLabel.Name = "DownloadLinkLabel";
-            DownloadLinkLabel.Size = new Size(365, 23);
+            DownloadLinkLabel.Size = new Size(730, 42);
             DownloadLinkLabel.TabIndex = 13;
             DownloadLinkLabel.TextAlign = ContentAlignment.MiddleLeft;
             DownloadLinkLabel.LinkClicked += DownloadLinkLabel_LinkClicked;
@@ -122,36 +129,40 @@
             // PatchInfoLabel
             // 
             PatchInfoLabel.BorderStyle = BorderStyle.Fixed3D;
-            PatchInfoLabel.Location = new Point(90, 67);
+            PatchInfoLabel.Location = new Point(180, 122);
+            PatchInfoLabel.Margin = new Padding(6, 0, 6, 0);
             PatchInfoLabel.Name = "PatchInfoLabel";
-            PatchInfoLabel.Size = new Size(269, 23);
+            PatchInfoLabel.Size = new Size(538, 42);
             PatchInfoLabel.TabIndex = 14;
             PatchInfoLabel.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // ReleaseLabel
             // 
             ReleaseLabel.BorderStyle = BorderStyle.Fixed3D;
-            ReleaseLabel.Location = new Point(90, 96);
+            ReleaseLabel.Location = new Point(180, 175);
+            ReleaseLabel.Margin = new Padding(6, 0, 6, 0);
             ReleaseLabel.Name = "ReleaseLabel";
-            ReleaseLabel.Size = new Size(269, 23);
+            ReleaseLabel.Size = new Size(538, 42);
             ReleaseLabel.TabIndex = 16;
             ReleaseLabel.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // groupBox1
             // 
             groupBox1.Controls.Add(LoggerListBox);
-            groupBox1.Location = new Point(12, 123);
+            groupBox1.Location = new Point(24, 224);
+            groupBox1.Margin = new Padding(6, 5, 6, 5);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(443, 169);
+            groupBox1.Padding = new Padding(6, 5, 6, 5);
+            groupBox1.Size = new Size(886, 308);
             groupBox1.TabIndex = 17;
             groupBox1.TabStop = false;
             groupBox1.Text = "日志信息";
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(14F, 31F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(468, 302);
+            ClientSize = new Size(936, 551);
             Controls.Add(groupBox1);
             Controls.Add(ReleaseLabel);
             Controls.Add(PatchInfoLabel);
@@ -164,6 +175,7 @@
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6, 5, 6, 5);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "MainForm";
